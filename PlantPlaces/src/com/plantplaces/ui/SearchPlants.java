17e5138c05897111a8146f84b53238fefc18cd56
Néli Fonseca <nelijfjr@gmail.com>
2018-02-24 01:46:59 -0300

    Aula23

--- PlantPlaces/src/com/plantplaces/ui/SearchPlants.java
+++ PlantPlaces/src/com/plantplaces/ui/SearchPlants.java
@@ -1,5 +1,8 @@
 package com.plantplaces.ui;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -31,4 +34,24 @@ public class SearchPlants {
 	public void setPlant(Plant plant) {
 		this.plant = plant;
 	}
+	
+	public List<Plant> completePlants(String query){
+		ArrayList<Plant> allPlants = new ArrayList<Plant>();
+		
+		//Create plants and add them to the collection
+		Plant redbul = new Plant();
+		redbul.setName("Eastern Redbul");
+		allPlants.add(redbul);
+		
+		Plant pawpaw = new Plant();
+		pawpaw.setName("Paw Paw");
+		allPlants.add(pawpaw);
+		
+		Plant nasturtiuim = new Plant();
+		nasturtiuim.setName("Nasturtiuium");
+		allPlants.add(nasturtiuim);
+		
+		return allPlants;
+	}
+	
 }
