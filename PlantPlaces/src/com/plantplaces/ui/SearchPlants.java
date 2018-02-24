7a24c78d131fe78e9b440a02170250998ac09d24
Néli Fonseca <nelijfjr@gmail.com>
2018-02-23 23:12:06 -0300

    Aula 19

---
+++ PlantPlaces/src/com/plantplaces/ui/SearchPlants.java
@@ -0,0 +1,34 @@
+package com.plantplaces.ui;
+
+import javax.faces.bean.ManagedBean;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.springframework.context.annotation.Scope;
+
+import com.plantplaces.dto.Plant;
+
+@Named
+@ManagedBean
+@Scope("session")
+public class SearchPlants {
+	
+	@Inject
+	private Plant plant;
+	
+	public String execute(){
+		if(plant != null && plant.getName().equalsIgnoreCase("Redbud")){
+			return "search";
+		}
+			
+		return "noresults";
+	}
+
+	public Plant getPlant() {
+		return plant;
+	}
+
+	public void setPlant(Plant plant) {
+		this.plant = plant;
+	}
+}
